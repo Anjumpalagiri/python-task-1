b627275d869f1f601f0c695cd37866f9ccc0ac61
Anjumpalagiri <anjum@retouchitservices.com>
2025-11-03 17:44:26 +0530

    Add files via upload

---
+++ python task 1.py
@@ -0,0 +1,5 @@
+#Asking for user's name and age
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+#printing greeting message
+print("Hello", name + "!", "You are", age, "years old.")
